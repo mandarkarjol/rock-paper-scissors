0533c1a0cc1732fdd2f5c8efb482195db48f172a
mandarkarjol <mandar.karjol@gmail.com>
2025-02-15 12:57:18 -0800

    Add js to html and function to get computer choice

---
+++ rps.js
@@ -0,0 +1,26 @@
+// Function to get Computer's choice
+function getComputerChoice(){
+    let computerChoice = ""; // Initiate variable to store choice
+    let randomNum = Math.floor(Math.random() * 3) // Generates whole random number between 0 and 2 inclusive 
+    
+    // Select Rock, Paper or Scissor based on random number generated in the previous step
+    if (randomNum === 0){
+        computerChoice = "rock";
+    }
+    else if (randomNum === 1){
+        computerChoice = "paper";
+    }
+    else{
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+
+// Function to get Human's choice
+
+// Declare varaibles to store Computer and Human scores
+
+// Define a game's round: Compare Computer's choice with Human's Choice and increment winner's score by 1
+
+// Play a game with five rounds and declare winner in the end
+
