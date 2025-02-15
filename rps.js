500856ed8797914b1bd5b916c938f2426d6de29f
mandarkarjol <mandar.karjol@gmail.com>
2025-02-15 13:00:22 -0800

    Add function to get human choice

--- rps.js
+++ rps.js
@@ -13,12 +13,31 @@ function getComputerChoice(){
     else{
         computerChoice = "scissors"
     }
+
     return computerChoice;
 }
 
 // Function to get Human's choice
+function getHumanChoice(){
+    let humanChoice = ""; // Initiate variable to store choice
+    let randomNum = Math.floor(Math.random() * 3) // Generates whole random number between 0 and 2 inclusive 
+    
+    // Select Rock, Paper or Scissor based on random number generated in the previous step
+    if (randomNum === 0){
+        humanChoice = "rock";
+    }
+    else if (randomNum === 1){
+        humanChoice = "paper";
+    }
+    else{
+        humanChoice = "scissors"
+    }
+    
+    return humanChoice;
+}
 
-// Declare varaibles to store Computer and Human scores
+// Declare variables to store Computer and Human scores
+
 
 // Define a game's round: Compare Computer's choice with Human's Choice and increment winner's score by 1
 
