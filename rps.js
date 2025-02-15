0550d835fff5284a3a57184f5573b25d51f23825
mandarkarjol <mandar.karjol@gmail.com>
2025-02-15 13:49:36 -0800

    Fix logic bug in the getHumanChoice function

--- rps.js
+++ rps.js
@@ -20,26 +20,38 @@ function getComputerChoice(){
 // Function to get Human's choice
 function getHumanChoice(){
     let humanChoice = ""; // Initiate variable to store choice
-    let randomNum = Math.floor(Math.random() * 3) // Generates whole random number between 0 and 2 inclusive 
+    let userInput = ""; // Initiate variable to store user input
+    let isInputValid = false; // Initiate boolean for input validation
     
-    // Select Rock, Paper or Scissor based on random number generated in the previous step
-    if (randomNum === 0){
-        humanChoice = "rock";
-    }
-    else if (randomNum === 1){
-        humanChoice = "paper";
-    }
-    else{
-        humanChoice = "scissors"
-    }
+    userInput = prompt("Enter your choice: rock, paper or scissors") // Prompts user for input
+
+    // Trim white space at the front or back of the input text if any
+    userInput = userInput.trim();
     
-    return humanChoice;
+    // Validate user input
+    // Check if input is valid
+    if ((userInput === "rock") || (userInput === "paper") || (userInput === "scissors")){
+        isInputValid = true; // Sets boolean to true
+    }
+
+    // Keep prompting if input is not valid
+    while(isInputValid === false){
+        userInput = prompt(`Invalid input: You entered "${userInput}". Please enter either rock, paper or scissors exactly without extra spaces, quotes or spelling errors`) // Prompts user for input again  
+        // Check if input is valid
+        if ((userInput === "rock") || (userInput === "paper") || (userInput === "scissors")){
+            isInputValid = true; // Sets boolean to true
+        } 
+    }
+    humanChoice = userInput; // Store validated input in variable to return
+    
+    return humanChoice    
 }
 
 // Declare variables to store Computer and Human scores
+let computerScore = 0;
+let humanScore = 0;
 
-
-// Define a game's round: Compare Computer's choice with Human's Choice and increment winner's score by 1
+// Function to play one round
 
 // Play a game with five rounds and declare winner in the end
 
