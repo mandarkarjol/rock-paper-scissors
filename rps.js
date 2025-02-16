53dac57f6513948b69c9762e8fda76776e92ec89
mandarkarjol <mandar.karjol@gmail.com>
2025-02-15 17:13:37 -0800

    Add playGame function

--- rps.js
+++ rps.js
@@ -66,58 +66,91 @@ function getHumanChoice(){
 }
 
 
-// Declare variables to store Computer and Human scores
+// Declare global variables to store Computer and Human scores
 let computerScore = 0;
 let humanScore = 0;
 
-// Function to play one round
-function playRound(humanChoice, computerChoice){
-    // Abort if user hits cancel
-    if (humanChoice == null){
-        console.log("User aborted");
-        return
-    }
 
-    // Initialize important variable
-    let isATie = false;
-
-    // Compare human choice to computer choice to determine winner, log a useful message and increment corresponding winner's score. Return isAtie as true if round is a tie.
-    if ((humanChoice === "rock") && (computerChoice === "scissors")){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        humanScore += 1;
-        return isATie;
-    }
-    else if ((humanChoice === "rock") && (computerChoice === "paper")){
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        computerScore += 1;
-        return isATie;
-    }
-    else if ((humanChoice === "paper") && (computerChoice === "rock")){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        humanScore += 1;
-        return isATie;
-    }
-    else if ((humanChoice === "paper") && (computerChoice === "scissors")){
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        computerScore += 1;
-        return isATie;
-    }
-    else if ((humanChoice === "scissors") && (computerChoice === "paper")){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        humanScore += 1;
-        return isATie;
-    }
-    else if ((humanChoice === "scissors") && (computerChoice === "rock")){
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        computerScore += 1;
-        return isATie;
-    }
-    else{
-        isATie = true;
-        console.log(`Both players chose ${humanChoice}.`)
-        return isATie;
-    }
-}
 
 // Play a game with five rounds and declare winner in the end
+function playGame(){
+    // Function to play one round
+    function playRound(humanChoice, computerChoice){
+        // Abort if user hits cancel
+        if (humanChoice == null){
+            console.log("User aborted");
+            return
+        }
 
+        // Initialize important variable
+        let isATie = false;
+
+        // Compare human choice to computer choice to determine winner, log a useful message and increment corresponding winner's score. Return isAtie as true if round is a tie.
+        if ((humanChoice === "rock") && (computerChoice === "scissors")){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+            humanScore += 1;
+            return isATie;
+        }
+        else if ((humanChoice === "rock") && (computerChoice === "paper")){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+            computerScore += 1;
+            return isATie;
+        }
+        else if ((humanChoice === "paper") && (computerChoice === "rock")){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+            humanScore += 1;
+            return isATie;
+        }
+        else if ((humanChoice === "paper") && (computerChoice === "scissors")){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+            computerScore += 1;
+            return isATie;
+        }
+        else if ((humanChoice === "scissors") && (computerChoice === "paper")){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+            humanScore += 1;
+            return isATie;
+        }
+        else if ((humanChoice === "scissors") && (computerChoice === "rock")){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+            computerScore += 1;
+            return isATie;
+        }
+        else{
+            isATie = true;
+            console.log(`Both players chose ${humanChoice}.`)
+            return isATie;
+        }
+    }
+    // Declare important variables
+    let roundCount = 0; // Counter to track # of rounds
+
+    // Play a round needs to be called until round counter reaches 5
+    while (roundCount < 5){
+        // Call playRound and store returned boolean in a variable
+        const isATie = playRound(getHumanChoice(),getComputerChoice());
+        // End game if user hits cancel on a prompt
+        if (isATie == null){
+            return
+        }
+        // Increment roundCount only if it is not a tie
+        if (!isATie){
+            roundCount += 1;
+        }
+    }
+
+    // Log the end of game
+    console.log("\n\nGame Over")
+
+    // Declare the winner
+    if (humanScore > computerScore){
+        console.log("Yay, you won!")
+    }
+    else {
+        console.log("Alas, the computer has won.")
+    }
+    // Show scores
+    console.log(`You: ${humanScore} | Computer: ${computerScore}`);
+}
+
+playGame();
