8c4ee42f9282add589b62531802a9318e197dc24
mandarkarjol <mandar.karjol@gmail.com>
2025-02-16 11:12:45 -0800

    Removes unnecessary comments and adds missing ";"
    Changes made in rps.js

--- rps.js
+++ rps.js
@@ -1,7 +1,6 @@
-// Function to get Computer's choice
 function getComputerChoice(){
-    let computerChoice = ""; // Initiate variable to store choice
-    let randomNum = Math.floor(Math.random() * 3) // Generates whole random number between 0 and 2 inclusive 
+    let computerChoice = "";
+    let randomNum = Math.floor(Math.random() * 3); // Generates whole random number between 0 and 2 inclusive 
     
     // Select Rock, Paper or Scissor based on random number generated in the previous step
     if (randomNum === 0){
@@ -11,78 +10,71 @@ function getComputerChoice(){
         computerChoice = "paper";
     }
     else{
-        computerChoice = "scissors"
-    }
+        computerChoice = "scissors";
+    };
 
     return computerChoice;
-}
+};
 
-// Function to get Human's choice
 function getHumanChoice(){
-    // Initiate important variables
     let humanChoice = "";
     let userInput = "";
     let isInputValid = false;
     
-    userInput = prompt("Enter your choice: rock, paper or scissors") // Prompts user for input
+    userInput = prompt("Enter your choice: rock, paper or scissors");
 
     // Abort if user hits cancel
     if (userInput === null){
-        return
-    }
+        return;
+    };
 
-    // Trim white space at the front or back of the input text if any
+    // Clean up user input
     userInput = userInput.trim();
-
-    // Convert all input to lower case
     userInput = userInput.toLowerCase();
     
     // Check if input is valid
     if ((userInput === "rock") || (userInput === "paper") || (userInput === "scissors")){
-        isInputValid = true; // Sets boolean to true
-    }
+        isInputValid = true;
+    };
 
     // Keep prompting if input is not valid
     while(isInputValid === false){
-        userInput = prompt(`Invalid input: You entered "${userInput}". Please enter either rock, paper or scissors exactly without extra spaces, quotes or spelling errors`)
+        userInput = prompt(`Invalid input: You entered "${userInput}". Please enter either rock, paper or scissors exactly without extra spaces, quotes or spelling errors`);
         
         // Abort if user hits cancel
         if (userInput == null){
-            return
-        }
+            return;
+        };
 
-        // Trim white space at the front or back of the input text if any
+        // Clean up user input
         userInput = userInput.trim();
-        // Convert all input to lower case
         userInput = userInput.toLowerCase();
 
         // Check if input is valid
         if ((userInput === "rock") || (userInput === "paper") || (userInput === "scissors")){
-            isInputValid = true; // Sets boolean to true
-        } 
-    }
-    humanChoice = userInput; // Store validated input in variable to return
-    return humanChoice    
-}
+            isInputValid = true;
+        }; 
+    };
+    humanChoice = userInput;
+    return humanChoice;    
+};
 
 
-// Declare global variables to store Computer and Human scores
+// Declare global variables
 let computerScore = 0;
 let humanScore = 0;
 
 
 
-// Play a game with five rounds and declare winner in the end
+// Play a game with max. five rounds and declare winner in the end
 function playGame(){
-    // Function to play one round
     function playRound(humanChoice, computerChoice){
         // Abort if user hits cancel
         if (humanChoice == null){
             console.log("User aborted");
-            return
-        }
+            return;
+        };
 
-        // Initialize important variable
         let isATie = false;
 
         // Compare human choice to computer choice to determine winner, log a useful message and increment corresponding winner's score. Return isAtie as true if round is a tie.
@@ -120,28 +112,24 @@ function playGame(){
             isATie = true;
             console.log(`Both players chose ${humanChoice}.`)
             return isATie;
-        }
-    }
-    // Declare important variables
-    let roundCount = 0; // Counter to track # of rounds
+        };
+    };
+    let roundCount = 0;
 
-    // Play a round needs to be called until round counter reaches 5
+    // Play a round needs to be called until round counter reaches 5 or either player reaches 3 wins
     while (roundCount < 5){
-        // Call playRound and store returned boolean in a variable
         const isATie = playRound(getHumanChoice(),getComputerChoice());
         // End game if user hits cancel on a prompt
         if (isATie == null){
             return
-        }
-        // Increment roundCount only if it is not a tie
+        };
         if (!isATie){
             roundCount += 1;
-        }
-        // Exit loop if one of the players is already at 3 wins
+        };
         if (humanScore === 3 || computerScore === 3){
             break;
-        }
-    }
+        };
+    };
 
     // Log the end of game
     console.log("\n\nGame Over")
@@ -152,9 +140,9 @@ function playGame(){
     }
     else {
         console.log("Alas, the computer has won.")
-    }
+    };
     // Show scores
     console.log(`You: ${humanScore} | Computer: ${computerScore}`);
-}
+};
 
 playGame();
